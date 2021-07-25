eadd8ae5138538ae664a131c395b92d27e3d2729
莫正华 <baisui@qlangtech.com>
2021-07-25 14:56:30 +0800

    add some error code for auto create clickhouse table when execute datax pipe

--- clickhousewriter/src/main/java/com/alibaba/datax/plugin/writer/clickhousewriter/ClickhouseWriterErrorCode.java
+++ clickhousewriter/src/main/java/com/alibaba/datax/plugin/writer/clickhousewriter/ClickhouseWriterErrorCode.java
@@ -3,29 +3,30 @@ package com.alibaba.datax.plugin.writer.clickhousewriter;
 import com.alibaba.datax.common.spi.ErrorCode;
 
 public enum ClickhouseWriterErrorCode implements ErrorCode {
-	TUPLE_NOT_SUPPORTED_ERROR("ClickhouseWriter-00", "不支持TUPLE类型导入."),
-	;
+    TUPLE_NOT_SUPPORTED_ERROR("ClickhouseWriter-00", "不支持TUPLE类型导入."),
+    REQUIRED_DATAX_PARAM_ERROR("ClickhouseWriter-01", "config param 'dataxName' is required"),
+    REQUIRED_TABLE_NAME_PARAM_ERROR("ClickhouseWriter-02", "config param 'tableName' is required");
 
-	private final String code;
-	private final String description;
+    private final String code;
+    private final String description;
 
-	private ClickhouseWriterErrorCode(String code, String description) {
-		this.code = code;
-		this.description = description;
-	}
+    private ClickhouseWriterErrorCode(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 
-	@Override
-	public String getCode() {
-		return this.code;
-	}
+    @Override
+    public String getCode() {
+        return this.code;
+    }
 
-	@Override
-	public String getDescription() {
-		return this.description;
-	}
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
 
-	@Override
-	public String toString() {
-		return String.format("Code:[%s], Description:[%s].", this.code, this.description);
-	}
+    @Override
+    public String toString() {
+        return String.format("Code:[%s], Description:[%s].", this.code, this.description);
+    }
 }
